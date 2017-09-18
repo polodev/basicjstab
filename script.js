a86406208650ee5f2946fd7c4e4b348e47f8948b
polo dev <polodev10@gmail.com>
2017-09-19 02:24:12 +0600

    getting done using data attribute

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+var tab = document.getElementsByClassName('tablink');
+for(var i=0; i < tab.length; i++) {
+  tab[i].addEventListener('click', function(evt) {
+    for(var j=0; j < tab.length; j++) {
+      tab[j].className = 'tablink';
+    }
+    evt.target.className += ' active';
+    var city = evt.target.getAttribute('data-city');
+    var tabcontent = document.getElementsByClassName('tabcontent');
+    for(var j=0; j < tabcontent.length; j++) {
+      tabcontent[j].className = 'tabcontent'
+    }
+    document.getElementById(city).className += ' active'
 
+  })
+}
 function openCity(evt, city) {
   var tab = document.getElementsByClassName('tablink');
   for(var i=0; i < tab.length; i++) {
